698635e24070b5b72c4f40ae742c8a30e7729a1a
Diegiwg <diegiwg@gmail.com>
2023-09-18 14:51:10 -0300

    replace bcrypt to bcryptjs

--- modules/Prisma.js
+++ modules/Prisma.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { safeParse } from "valibot";
 
 import { SessionSchema } from "./Models.js";
