1506fe553a544d94f1b0ee746d3e6b5a7872e5cc
Diego Queiroz <diegiwg@gmail.com>
2023-09-20 01:13:29 -0300

    Adicionado botão para ir a pagina de criação de personagem

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -1,6 +1,10 @@
 import { EmitEvent, Page } from "../../modules/Functions.js";
 
 export function mount() {
+    Page.querySelector("#CreateCharacter").onclick = () => {
+        EmitEvent("RequestPage", { page: "CreateCharacter" });
+    };
+
     Page.querySelector("#Logout").onclick = () => {
         EmitEvent("Logout");
     };
